f9b2216b4e01e0c338b8823d34ed64dfcdee016e
liyongqiang1210 <1240965061@qq.com>
2018-10-03 17:28:18 +0800

    添加异常判断

--- src/main/java/com/spider/wenshu_spider/WenShuSpider.java
+++ src/main/java/com/spider/wenshu_spider/WenShuSpider.java
@@ -1,6 +1,5 @@
 package com.spider.wenshu_spider;
 
-import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 
@@ -8,6 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import cn.edu.hfut.dmic.webcollector.crawldb.DBManager;
 import cn.edu.hfut.dmic.webcollector.crawler.Crawler;
@@ -30,8 +30,12 @@ import cn.edu.hfut.dmic.webcollector.plugin.berkeley.BerkeleyDBManager;
 public class WenShuSpider implements Executor {
 
 	public void execute(CrawlDatum datum, CrawlDatums next) throws Exception {
+		// 设置代理IP
+		ChromeOptions options = new ChromeOptions();
+		// Proxy proxy = new Proxy();
+		// proxy.setHttpProxy("61.135.217.7:80222");
 		// 创建chrome浏览器驱动
-		WebDriver driver = new ChromeDriver();
+		WebDriver driver = new ChromeDriver(options);
 		// 最大化浏览器窗口否则会造成找不到下一页按钮的bug
 		driver.manage().window().maximize();
 		// 打开链接
@@ -39,27 +43,41 @@ public class WenShuSpider implements Executor {
 		// 创建一个随机数对象
 		Random random = new Random();
 		Thread.sleep(5000);
-		// 获取下一页按钮
-		WebElement nextElement = driver.findElement(By.className("next"));
-		while (nextElement != null) {
-			// 获取到我们需要的数据集合
-			List<WebElement> Elements = driver.findElements(By.className("dataItem"));
-			for (Iterator<WebElement> iterator = Elements.iterator(); iterator.hasNext();) {
-				WebElement webElement = (WebElement) iterator.next();
-				String text = webElement.getText();
-				System.out.println(text);
+
+		try {
+			// 获取下一页按钮
+			WebElement nextElement = driver.findElement(By.className("next"));
+			while (nextElement != null) {
+				// 获取到我们需要的数据集合
+				List<WebElement> Elements = driver.findElements(By.className("dataItem"));
+				// for (Iterator<WebElement> iterator = Elements.iterator();
+				// iterator.hasNext();) {
+				// WebElement webElement = (WebElement) iterator.next();
+				// String text = webElement.getText();
+				// System.out.println(text);
+				// }
+				// 随机等待
+				Thread.sleep(5000 - random.nextInt(2000));
+				// 模拟浏览器点击下一页操作
+				nextElement.click();
+				// 随机等待
+				Thread.sleep(5000 - random.nextInt(1000));
+				// 重新获取刷新页面后的下一页按钮
+				while (true) {
+					try {
+						nextElement = driver.findElement(By.className("next"));
+						System.out.println("找到下一页");
+						break;
+					} catch (Exception e) {
+						System.out.println("未找到下一页");
+					}
+				}
 			}
-			// 随机等待
-			Thread.sleep(5000 - random.nextInt(2000));
-			// 模拟浏览器点击下一页操作
-			nextElement.click();
-			// 随机等待
-			Thread.sleep(5000 - random.nextInt(1000));
-			// 重新获取刷新页面后的下一页按钮
-			nextElement = driver.findElement(By.className("next"));
+		} catch (Exception e) {
+			System.out.println(e);
 		}
 		// 关闭浏览器
-		driver.close();
+		driver.quit();
 	}
 
 	public static void main(String[] args) throws Exception {

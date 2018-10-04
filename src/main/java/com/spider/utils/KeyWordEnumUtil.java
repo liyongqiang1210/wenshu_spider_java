afb081f385396b0ad0333b97f6f51cd2f8e0ab8c
liyongqiang1210 <1240965061@qq.com>
2018-10-04 17:31:01 +0800

    添加关键字枚举类

---
+++ src/main/java/com/spider/utils/KeyWordEnumUtil.java
@@ -0,0 +1,32 @@
+package com.spider.utils;
+
+public class KeyWordEnumUtil {
+
+	public enum CaseTypeEnum {
+		CAIVILCASE {
+			public String[] getName() {
+				return null;
+			}
+		},
+		CRIMINALCASE {
+			public String[] getName() {
+				return null;
+			}
+		},
+		ADMINISTRATIVECASE {
+			public String[] getName() {
+				return null;
+			}
+		},
+		COMPENSATECASE {
+			public String[] getName() {
+				return null;
+			}
+		},
+		IMPLEMENTCASE {
+			public String[] getName() {
+				return null;
+			}
+		};
+	}
+}
